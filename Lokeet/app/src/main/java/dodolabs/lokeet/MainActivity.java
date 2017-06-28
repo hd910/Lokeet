6fafe4ed7ff4516f42d3a93e9f552f207891c581
hd910 <hd910@outlook.com>
2017-06-28 14:43:19 +1200

    Add SharedPreferences storage

--- Lokeet/app/src/main/java/dodolabs/lokeet/MainActivity.java
+++ Lokeet/app/src/main/java/dodolabs/lokeet/MainActivity.java
@@ -1,13 +1,20 @@
 package dodolabs.lokeet;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView countText;
+    private int unlockCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +30,17 @@ public class MainActivity extends AppCompatActivity {
         //Removes the KeyGuard all together (no security)
         //this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
         this.getWindow().setType(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
-
         setContentView(R.layout.activity_main);
+
+        SharedPreferences prefs = this.getSharedPreferences("unlock_count", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        unlockCount = prefs.getInt("count", 0);
+        unlockCount++;
+        editor.putInt("count", unlockCount);
+        editor.apply();
+
+        countText = (TextView)findViewById(R.id.unlock_count);
+        countText.setText(Integer.toString(unlockCount));
     }
 
     private void makeFullScreen() {
@@ -44,8 +60,14 @@ public class MainActivity extends AppCompatActivity {
         return; //Don't Do Anything
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
     //Unlock screen
     public void unlockScreen(View view) {
+        unlockCount++;
         //Instead of using finish(), destroys the process
         android.os.Process.killProcess(android.os.Process.myPid());
     }
